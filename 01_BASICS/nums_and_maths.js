bf07abf7a42ed23e88edc44872a141f25936be71
arbaazalishaikh <123721449+arbaazalishaikh@users.noreply.github.com>
2025-05-05 06:43:08 +0000

    num and math

---
+++ 01_BASICS/nums_and_maths.js
@@ -0,0 +1,37 @@
+// Numbers
+
+// const score = 100;
+// console.log(score);
+
+// const num = new Number(400); // explicitly humne bataya ki Data type Number hona chahiye
+// console.log(num);
+
+// const balance = 4000
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3)); // decimal point add hojayega 
+
+// const num1 = 223.789
+// console.log(num1.toPrecision(3)); // round off krk milega
+
+// const num2 = 1000000
+// console.log(num2.toLocaleString()); 
+// console.log(num2.toLocaleString('en-IN'));
+
+// Math
+
+// console.log(Math.PI);
+// console.log(Math.abs(-5)); // 5
+// console.log(Math.round(4.5)); // 5
+// console.log(Math.ceil(5.6)); // top value - 6
+// console.log(Math.floor(5.6)); // low value - 5
+
+// console.log(Math.min(4,5,63,6)); // 4
+// console.log(Math.max(4,5,67,8)); // 67
+
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
